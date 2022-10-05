08f1c278438497877a30072fc3882fe66d1ac1c7
Guilherme Rio-Tinto <guilherme.rio-tinto@kcl.ac.uk>
2022-10-05 12:22:42 +0100

    Add prime check method and fix primes method

--- primes.py
+++ primes.py
@@ -1,6 +1,23 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def checkPrime(num):
+    if num == 2:
+        return True
+    if num > 2:
+        for n in range(2, num):
+            if (num % n) == 0:
+                return False
+            return True
+        else:
+            return False
+
 def primes(number_of_primes):
     list = []
+    n = 2
+    while len(list) != number_of_primes:
+        if checkPrime(n):
+            list.append(n)
+        n +=1
+
     return list
